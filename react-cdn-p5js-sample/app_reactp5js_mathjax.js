d3e623003f7f55df571c224f7737ca38fe21692f
wamtjp <stormymath0@gmail.com>
2025-07-31 11:26:44 +0900

    Add app_reactp5js_mathjax 2

--- react-cdn-p5js-sample/app_reactp5js_mathjax.js
+++ react-cdn-p5js-sample/app_reactp5js_mathjax.js
@@ -1,6 +1,7 @@
 function Sketch() {
   React.useEffect(() => {
-    let sketch = (p) => {
+    // p5.jsの初期化
+    const sketch = (p) => {
       p.setup = () => {
         p.createCanvas(400, 200);
         p.background(240);
@@ -10,22 +11,27 @@ function Sketch() {
         p.ellipse(p.mouseX, p.mouseY, 20, 20);
       };
     };
-    new p5(sketch, document.getElementById('sketch-container'));
+    new p5(sketch, document.getElementById("sketch-holder"));
+
+    // MathJaxの再レンダリング（重要）
+    if (window.MathJax && window.MathJax.typeset) {
+      window.MathJax.typeset();
+    }
   }, []);
 
   return (
     <div>
-			<p>上の数式: <span>{"$y = ax^2 + bx + c$"}</span></p>
-			<div id="sketch-holder"></div>
-			<p>下の数式: <span>{"$\\frac{dy}{dx} = 2ax + b$"}</span></p>
+      <p>上の数式: <span>{"$y = ax^2 + bx + c$"}</span></p>
+      <div id="sketch-holder"></div>
+      <p>下の数式: <span>{"$\\frac{dy}{dx} = 2ax + b$"}</span></p>
     </div>
-	);
+  );
 }
 
 function App() {
   return (
     <div>
-      <h1>React-CDN + p5.js Sample</h1>
+      <h1>React-CDN + p5.js + MathJax Sample</h1>
       <Sketch />
     </div>
   );
